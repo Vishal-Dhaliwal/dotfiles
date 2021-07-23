514821849020018922111a2c2f69d71a95717f75
Vishal-Dhaliwal <69068697+Vishal-Dhaliwal@users.noreply.github.com>
2021-07-23 09:24:07 +0530

    Add files via upload

---
+++ dwm/autostart/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#~/.local/bin/3-monitors.sh &
+# wal -R &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+nitrogen --restore &
+compton --config ~/.config/compton/compton.conf &
+# xsetroot -cursor_name left_ptr
+# xsetroot -name "Vishaldeep Singh"
+slstatus &
+# nm-applet &
